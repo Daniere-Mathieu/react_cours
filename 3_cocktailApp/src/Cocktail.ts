862a6319649a2674610c800dc07602f4aef40b74
Daniere Mathieu <mathieudaniere@orange.fr>
2023-11-02 10:20:58 +0100

    crying because the interface for the response is horrible due to the API :'(

--- 3_cocktailApp/src/Cocktail.ts
+++ 3_cocktailApp/src/Cocktail.ts
@@ -1,5 +1,5 @@
 import { CocktailEndpoints } from "./enums/CocktailEndpoints";
-
+import { Category } from "./enums/Category";
 export class Cocktail {
   private _urlPath: string = "https://www.thecocktaildb.com/api/json/v1/1/";
 
@@ -14,7 +14,14 @@ export class Cocktail {
     return data;
   }
 
-  async getByCategory() {
-    this._fetchApi(CocktailEndpoints.FILTERBYCATEGORY);
+  async getByCategory(category: string) {
+    if (
+      !category &&
+      !Object.values(Category).includes(category as unknown as Category)
+    )
+      return null;
+    const result = this._fetchApi(
+      CocktailEndpoints.FILTER_BY_CATEGORY + category
+    );
   }
 }
